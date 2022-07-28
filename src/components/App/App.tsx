d48dfe6f03b3bb49954a733e01938809debd7dbc
Edgallen <popov567@gmail.com>
2022-07-28 15:12:22 +0300

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './App.module.css';
+
+const App = () => {
+
+  return (
+      <h1 className={styles.title}>Hello world</h1>
+  );
+};
+
+export default App;
